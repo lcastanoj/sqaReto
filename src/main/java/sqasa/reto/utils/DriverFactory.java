700379e6ee2b87ce80bf10c73fb4f745b5c7c393
Lucas Castaño Jaramillo <38567515+lcastanoj@users.noreply.github.com>
2025-11-27 12:13:24 -0500

    Update DriverFactory.java

--- src/main/java/sqasa/reto/utils/DriverFactory.java
+++ src/main/java/sqasa/reto/utils/DriverFactory.java
@@ -1,16 +1,31 @@
 package sqasa.reto.utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
 
     public static WebDriver getDriver() {
-        WebDriverManager.chromedriver().clearDriverCache().setup();
+        WebDriverManager.chromedriver().setup();
+
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--start-maximized");
+
+        // Detectar si estamos en CI (GitHub Actions)
+        boolean isCI = System.getenv("CI") != null || System.getenv("GITHUB_ACTIONS") != null;
+
+        if (isCI) {
+            chromeOptions.addArguments("--headless=new");   // modo headless moderno
+            chromeOptions.addArguments("--no-sandbox");
+            chromeOptions.addArguments("--disable-dev-shm-usage");
+            chromeOptions.addArguments("--disable-gpu");
+            chromeOptions.addArguments("--window-size=1920,1080");
+        } else {
+            chromeOptions.addArguments("--start-maximized");
+        }
+
+        // Opciones comunes
         chromeOptions.addArguments("--disable-notifications");
         chromeOptions.addArguments("--disable-popup-blocking");
         chromeOptions.addArguments("--disable-infobars");
